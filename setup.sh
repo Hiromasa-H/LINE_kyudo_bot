4d690c52f3a34f943825710f3220687b11f9a1e9
hhiromasa <hhiromasa217@gmail.com>
2023-06-10 21:47:43 +0900

    added echo current url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 touch .env
-echo "LINE_CHANNEL_SECRET='<your token here>'" >> .env
+echo "LINE_CHANNEL_SECRET='<your token here>' \n CURRENT_URL = '<deployed URL here>' " >> .env
 pip3 install -r requirements.txt
 python -m pip3 install 'git+https://github.com/facebookresearch/detectron2.git'
